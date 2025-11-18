aa5c1f0fff4147e8433fa59b177b94151fb76ea4
salonijibhe07 <salonijibhe07@gmail.com>
2025-11-18 09:49:53 +0530

    Import sucessfull

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
-import {ClearkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";   // ✅ FIXED
 
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
@@ -14,15 +14,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <ClearkProvider>
-        <html lang="en">
-            <body className={`${outfit.className} antialiased`}>
-                <StoreProvider>
-                    <Toaster />
-                    {children}
-                </StoreProvider>
-            </body>
-        </html>
-        </ClearkProvider>
+        <ClerkProvider>    {/* ✅ FIXED */}
+            <html lang="en">
+                <body className={`${outfit.className} antialiased`}>
+                    <StoreProvider>
+                        <Toaster />
+                        {children}
+                    </StoreProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
